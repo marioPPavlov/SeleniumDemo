582027a8db4010cb582d969023f5ecf71ab77aab
marioPPavlov <mario.pavlov.pavlov@gmail.com>
2018-06-14 12:19:25 +0300

    Added Registration and Login tests

---
+++ SeleniumDemo.Tests/RegisterLoginTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using SeleniumDemo.TestFramework.Pages;
+using SeleniumDemo.TestFramework;
+
+namespace SeleniumDemo.Tests
+{
+    public class RegisterLoginTests : BaseTest
+    {
+        public RegisterLoginTests()
+        {
+        }
+
+        [Test]
+        [Order(1)]
+        public void Register()
+        {
+            RegisterPage regPage = new RegisterPage(driver);
+            regPage.Goto();
+            regPage.RegisterNewUser();
+
+            Assert.IsTrue(regPage.IsUserLoggedIn());
+        }
+
+        [Test]
+        public void LogIn()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.Goto();
+            loginPage.LogIn();
+
+            Assert.IsTrue(loginPage.IsUserLoggedIn());
+        }
+
+        [Test]
+        public void LogOut()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.Goto();
+            loginPage.LogIn();
+
+            Assert.IsTrue(loginPage.IsUserLoggedIn());
+            loginPage.LogOut();
+            Assert.IsFalse(loginPage.IsUserLoggedIn());
+        }
+    }
+}
