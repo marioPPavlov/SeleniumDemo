14d84b8a38c3093a60898fbdb8511826a76c5c3a
marioPPavlov <mario.pavlov.pavlov@gmail.com>
2018-06-14 10:24:23 +0300

    Added Pages in .TestFramework

---
+++ SeleniumDemo.TestFramework/Pages/LoginPage.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using SeleniumTest.Generators;
+
+namespace SeleniumDemo.TestFramework.Pages
+{
+    public class LoginPage : BasePage
+    {
+        public LoginPage(RemoteWebDriver driver) : base(driver)
+        {
+        }
+
+        private IWebElement LoginLink { get => topNavigationBar.LogInLink(); }
+        private IWebElement EmailAddressTextField { get => driver.FindElementById("Email"); }
+        private IWebElement PasswordTextField { get => driver.FindElementById("Password"); }
+        private IWebElement LogInButton { get => driver.FindElementByName("LogInButton"); }
+        private IWebElement LogOutButton { get => topNavigationBar.LogOutLink(); }
+
+        public override void Goto()
+        {
+            LoginLink.Click();
+        }
+
+        public void LogIn()
+        {
+            EmailAddressTextField.SendKeys(UserGenerator.LastGeneratedUser().EmailAddress);
+            PasswordTextField.SendKeys(UserGenerator.LastGeneratedUser().Password);
+            LogInButton.Click();
+        }
+
+        public void LogOut()
+        {
+            LogOutButton.Click();
+        }
+
+
+    }
+}
---
+++ SeleniumDemo.TestFramework/Pages/Navigation/TopNavigationBar.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeleniumDemo.TestFramework.Pages.Navigation
+{
+    public class TopNavigationBar
+    {
+        protected RemoteWebDriver driver;
+        public TopNavigationBar(RemoteWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public IWebElement LogInLink()
+        {
+            return driver.FindElementByName("Login");
+        }
+
+        public IWebElement LogOutLink()
+        {
+            return driver.FindElementByName("LogOut");
+        }
+
+        public IWebElement RegisterLink()
+        {
+            return driver.FindElementByName("Register");
+        }
+
+        public IWebElement LoggedUser()
+        {
+            return driver.FindElementByName("loggedUser");
+        }
+
+    }
+}
